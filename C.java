738548778d2b2139c8fadb9f88609c5c0ad2d379
mandyam Thanuvigna <96224107+Thanuvi@users.noreply.github.com>
2022-02-04 10:50:16 +0530

    Add files via upload

---
+++ C.java
@@ -0,0 +1,15 @@
+package programs;
+
+class A{
+    void msg(){System.out.println("Hello");}
+}
+class B{
+    void msg(){System.out.println("Welcome");}
+}
+class C extends A,B {
+
+    public static void main(String args[]) {
+        C obj = new C();
+        obj.msg();
+    }
+}
---
+++ StringIndexOutOfBound.java
@@ -0,0 +1,15 @@
+package programs;
+
+public class StringIndexOutOfBound {
+    public static void main(String args[])
+    {
+        try {
+            String a = "This is like chipping "; // length is 22
+            char c = a.charAt(23); // accessing 25th element
+            System.out.println(c);
+        }
+        catch(StringIndexOutOfBoundsException e) {
+            System.out.println("StringIndexOutOfBoundsException");
+        }
+    }
+}
